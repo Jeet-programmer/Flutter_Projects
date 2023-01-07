7c48b4f5f9d52efaeb603984d27c9a9ea94262e5
Jeet Ghosh <jeetghoshsci@gmail.com>
2023-01-07 10:01:11 +0530

    First Comit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Hello World"),
+          ),
+        ),
+      ),
+    );
+  }
+}
